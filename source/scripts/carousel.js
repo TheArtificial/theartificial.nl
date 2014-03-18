267284d05fe2cb9b811ed7d95245672c73b9998d
Hans Gerwitz <hans@gerwitz.com>
2014-03-18 10:38:45 +0100

    Button fixes and code cleanup

---
+++ source/scripts/carousel.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  var carouselPrefix = '#carousel ';
+  
+  $(carouselPrefix+'.scrolling').scrollsnap({
+      direction: 'x',
+      snaps: carouselPrefix+'.scrolling-wrapper li',
+      proximity: 1000,
+      latency: 200,
+      duration: 500,
+      easing: 'easeInOutCirc'
+  });
+  $(carouselPrefix+'#next').on('click', function() {
+    $(carouselPrefix+'.scrolling').trigger('next');
+  });
+  $(carouselPrefix+'#prev').on('click', function() {
+    $(carouselPrefix+'.scrolling').trigger('prev');
+  });
+  $(carouselPrefix+'.scrolling').on('scrollstart', function() {
+    $('.casestudies a').removeClass('current');
+    $('.overlay').removeClass('current');
+  });
+  $(carouselPrefix+'.scrolling-wrapper li').on('scrollstopped', function() {
+    $('.casestudies a').removeClass('current');
+    menuSelector = this.dataset.menu;
+    $(menuSelector).addClass('current');
+    $('.overlay').removeClass('current');
+    overlaySelector = this.dataset.overlay;
+    $(overlaySelector).addClass('current');
+  });
+
+});
