086852524b3e78fd4eb4de600b8b788d13858645
shannon e thomas <theoneandonly@shannonethomas.com>
2015-04-06 10:27:51 +0200

    blog

--- config.rb
+++ config.rb
@@ -47,14 +47,61 @@
 #   end
 # end
 
+helpers do
+  def nav_link(link_text, url, options = {})  
+    options[:class] ||= ""
+    options[:class] << " current" if current_resource.url.start_with?(url)
+    link_to(link_text, url, options)
+  end
+  
+  def find_image(base_path)
+    if found = sitemap.find_resource_by_path(base_path+'.png')
+      return found
+    elsif found = sitemap.find_resource_by_path(base_path+'.jpg')
+      return found
+    else
+      return nil
+    end
+  end
+end
+
 set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+Time.zone = "Amsterdam"
+
+activate :blog do |blog|
+  blog.prefix = ""
+  blog.sources = "/blog/{year}-{month}-{day}-{title}.html"
+  blog.permalink = "/blog/{year}/{month}/{day}/{title}.html"
+  blog.layout = "blog_layout"
+  blog.default_extension = ".md"
+  blog.summary_separator = /READMORE/
+  blog.paginate = true
+#  blog.custom_collections = {
+#    author: {
+#      link: '/people/{author}.html',
+#      template: '/people/template.html'
+#    }
+#  }
+end
+page "/blog/feed.xml", layout: false
+
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true,
+               :autolink => true, 
+               :smartypants => true
+
 # Build-specific configuration
 configure :build do
+  activate :minify_css
+  activate :minify_javascript, ignore: 'jquery.artificial.logo.js'
+  activate :gzip
+  default_caching_policy public: true, must_revalidate: true
+
   # For example, change the Compass output style for deployment
   # activate :minify_css
 
