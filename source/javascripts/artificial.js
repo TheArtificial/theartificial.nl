27ba8dadd801b663d7eac9a9e9424d5b61557d06
Hans Gerwitz <robots@theartificial.nl>
2016-02-07 13:49:34 +0100

    Fix scrollspy to use offset for anchors

--- source/javascripts/artificial.js
+++ source/javascripts/artificial.js
@@ -1,4 +1,4 @@
-// this binds navigation items to items within the body 
+// this binds navigation items to items within the body
 // thanks https://jsfiddle.net/mekwall/up4nu/
 function scrollspy(selector, heightOffset) {
 	var lastId,
@@ -14,7 +14,7 @@ function scrollspy(selector, heightOffset) {
 	  var href = $(this).attr("href"),
 	      offsetTop = href === "#" ? 0 : $(href).offset().top+1;
     $("html, body").stop().animate({
-      scrollTop: offsetTop
+      scrollTop: (offsetTop - heightOffset)
     }, 300);
     e.preventDefault();
 	});
@@ -23,7 +23,7 @@ function scrollspy(selector, heightOffset) {
 	$(window).scroll(function(){
 	  // Get container scroll position
 	  var fromTop = $(this).scrollTop();
-   
+
 	  // Get id of current scroll item
 	  var cur = scrollItems.map(function(){
 	    if ($(this).offset().top < fromTop)
@@ -32,53 +32,53 @@ function scrollspy(selector, heightOffset) {
 	  // Get the id of the current element
 	  cur = cur[cur.length-1];
 	  var id = cur && cur.length ? cur[0].id : "";
-   
+
 	  if (lastId !== id) {
       lastId = id;
       menuItems
         .parent().removeClass("active")
         .end().filter("[href=#"+id+"]").parent().addClass("active");
 	  }
-	});	
+	});
 }
 
 // this brings up search by typing
 function eversearch() {
 	// Detect keystroke and present search
 	var $searchField = $("#oncall .search .searchbox");
-	
-	function hideSearch(skipblur) { 
+
+	function hideSearch(skipblur) {
 		$("#oncall").css("display","none");
 		if (!skipblur) {
 			$searchField.blur();
 		}
 	}
-	
+
 	function showSearch() {
 		$("#oncall").css("display","block");
-		$searchField.focus();	
+		$searchField.focus();
 	}
-	
+
 	$(window).keypress(function(e) {
 		if ($(e.target).is('input, textarea')) {
 			return;
 		}
-		
+
 		var code = e.charCode || e.which;
-		
+
 		switch (code) {
 			case 8:
 			case 27:
 				// do nothing
 				break;
 			default:
-				showSearch();	
+				showSearch();
 		}
 	});
-	
+
 	$searchField.keydown(function(e) {
 		var code = e.charCode || e.which;
-		
+
 		// exit upon esc
 		if (code == 27) {
 	 		hideSearch();
@@ -88,7 +88,7 @@ function eversearch() {
 			hideSearch();
 		}
 	});
-	
+
 	$searchField.blur(function(e) {
 		$searchField.val("");
 		hideSearch(true);
@@ -105,13 +105,13 @@ function listfilter() {
     	if (filterItem == "all") {
 				return false;
 			}
-			else { 
+			else {
 				return true;
 			}
     });
 		filteredFilters.unshift("div");
 		var filterSelector = filteredFilters.join(".");
-    
+
 		$("div#animal-list .col-3").find("div").addClass("filter-off");
     $("div#animal-list .col-3").find(filterSelector).removeClass("filter-off");
 	});
