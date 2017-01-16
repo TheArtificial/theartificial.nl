983330e92e6bed314ccb4d64460e8cdcc334d0ef
Hans Gerwitz <robots@theartificial.nl>
2017-01-16 09:03:25 +0100

    leave the tmp directory alone

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-aws s3 sync build s3://theartificial.nl --region eu-west-1 --acl public-read --delete --cache-control "public, max-age=3600"
+aws s3 sync build s3://theartificial.nl --region eu-west-1 --acl public-read --delete --exclude "tmp/*" --cache-control "public, max-age=3600"
