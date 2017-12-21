6cee9a1f1af49d9fc2999eb0445853ef4e9247c5
Hans Gerwitz <hans@gerwitz.com>
2017-12-21 13:18:32 +0100

    Script redirects

---
+++ set-redirects.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+aws s3api put-object --bucket theartificial.nl --region eu-west-1 --acl public-read --website-redirect-location "/work/3DSystems-consumer.html"  --key "3dsystems.html"
+aws s3api put-object --bucket theartificial.nl --region eu-west-1 --acl public-read --website-redirect-location "/laboratory/futureofdeath.html"  --key "designfordeath.html"
+aws s3api put-object --bucket theartificial.nl --region eu-west-1 --acl public-read --website-redirect-location "/cityguide/"  --key "travelguide/"
